79e8c44b7c6986d32163444ac45f8ba5342f51f4
Lildark31 <128464699+Lildark31@users.noreply.github.com>
2023-03-28 09:39:01 +0200

    Update m5stack-myclock.ino

--- m5stack-myclock/m5stack-myclock.ino
+++ m5stack-myclock/m5stack-myclock.ino
@@ -6,16 +6,15 @@
 
 const char *ssid = "Nom_du_réseaux";
 const char *password = "mots_de_passe";
-int timeBefore = 0;
+const long utcOffsetInSeconds = 3600; // Décalage horaire en secondes (pour la France)
 
-const long utcOffsetInSeconds = 2 * 60 * 60;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 //------------------------------------- Moteur pas a pas (mpap)
-const int stepperPasTour = 2048;  // Nombre de pas par tour
-#define mpapFilBleu 16            // Pour cablage
+const int stepperPasTour = 2048;     // Nombre de pas par tour
+#define mpapFilBleu 16     // Pour cablage
 #define mpapFilRose 17
 #define mpapFilJaune 2
 #define mpapFilOrange 5
@@ -24,13 +23,14 @@ Stepper mpap = Stepper(stepperPasTour, mpapFilBleu, mpapFilJaune, mpapFilRose, m
 
 void setup() {
   Serial.begin(115200);
+  delay(3000);
   M5.begin();
   WiFi.begin(ssid, password);
   M5.Power.begin();
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay (500);
+    Serial.print ( "." );
   }
 
   mpap.setSpeed(15);
@@ -40,48 +40,29 @@ void setup() {
 void loop() {
   timeClient.update();
 
-  if (timeClient.getSeconds() != timeBefore) {
-    M5.Lcd.setTextSize(6);
-
-    M5.Lcd.setCursor(20, 90);
-    M5.Lcd.setTextColor(TFT_BLACK, TFT_BLACK);
-    M5.Lcd.print(timeBefore);
-
-    M5.Lcd.setCursor(20, 90);
-    M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
-    M5.Lcd.println(timeClient.getFormattedTime());
-
-    timeBefore = timeClient.getSeconds();
-
-    Serial.println("MPAP action " + String(timeClient.getSeconds()));
-    mpap.step(stepperPasTour / 60);
-  }
-}
-
-//affiche la date
-void loop() {
-  timeClient.update();
-  if (timeClient.getSeconds() != timeBefore) {
-    M5.Lcd.setTextSize(6);
-    M5.Lcd.setCursor(20, 90);
-    M5.Lcd.setTextColor(TFT_BLACK, TFT_BLACK);
-    M5.Lcd.print(timeBefore);
-
-    M5.Lcd.setCursor(20, 90);
-    M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
-    M5.Lcd.println(timeClient.getFormattedTime());
-
-    // Ajout pour afficher la date
+  if (timeClient.getSeconds() != timeBefore)
+  {
     M5.Lcd.setTextSize(4);
-    M5.Lcd.setCursor(20, 200);
-    M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
-    M5.Lcd.print(String(timeClient.getDay()) + "/" + String(timeClient.getMonth()) + "/" + String(timeClient.getYear()));
+    
+    M5.Lcd.setCursor(75, 90);
+    M5.Lcd.setTextColor(TFT_BLUE, TFT_BLACK);
+    M5.Lcd.println(timeClient.getFormattedTime());
+
+    // Récupère la date actuelle à partir de timeClient.getEpochTime()
+    time_t now = timeClient.getEpochTime();
+    struct tm timeinfo;
+    localtime_r(&now, &timeinfo);
+
+    // Affiche la date sur l'écran
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(75, 140);
+    M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+    M5.Lcd.printf("%02d/%02d/%d", timeinfo.tm_mday, timeinfo.tm_mon + 1, timeinfo.tm_year + 1900);
 
     timeBefore = timeClient.getSeconds();
-
+    
     Serial.println("MPAP action " + String(timeClient.getSeconds()));
-    mpap.step(stepperPasTour / 60);
-  }
-}
+    mpap.step(stepperPasTour/60);
 
-//pictogramme de la méteo
+   }
+}
